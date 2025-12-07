dd9463db499326e8a68643bacedece6c0191582f
Sairiz <1634949+sairiz@users.noreply.github.com>
2025-12-07 18:13:34 +0800

    vouchers

--- packages/vouchers/src/Events/VoucherRemoved.php
+++ packages/vouchers/src/Events/VoucherRemoved.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace AIArmada\Vouchers\Events;
 
 use AIArmada\Cart\Cart;
+use AIArmada\CommerceSupport\Contracts\Events\VoucherEventInterface;
 use AIArmada\Vouchers\Data\VoucherData;
+use AIArmada\Vouchers\Events\Concerns\HasVoucherEventData;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -13,9 +15,10 @@ use Illuminate\Queue\SerializesModels;
 /**
  * Fired when a voucher is removed from a cart.
  */
-class VoucherRemoved
+class VoucherRemoved implements VoucherEventInterface
 {
     use Dispatchable;
+    use HasVoucherEventData;
     use InteractsWithSockets;
     use SerializesModels;
 
@@ -28,5 +31,15 @@ class VoucherRemoved
     public function __construct(
         public readonly Cart $cart,
         public readonly VoucherData $voucher
-    ) {}
+    ) {
+        $this->initializeEventData();
+    }
+
+    /**
+     * Get the event type identifier.
+     */
+    public function getEventType(): string
+    {
+        return 'voucher.removed';
+    }
 }
