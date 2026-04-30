a1d927f7903c986b7fede13ddb0cc720572a2c93
Sairiz <1634949+sairiz@users.noreply.github.com>
2026-05-01 00:09:04 +0800

    chore(demo): remove storage from tracking, add to .gitignore

--- demo/storage/framework/views/ea68d3e89d80a1cee636195981fbe090.php
+++
@@ -1,8 +0,0 @@
-<div
-    <?php echo e($attributes->gridColumn($this->getColumnSpan(), $this->getColumnStart())->class(['fi-wi-widget'])); ?>
-
->
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /Users/saiffil/Herd/commerce/demo/vendor/filament/widgets/resources/views/components/widget.blade.php ENDPATH**/ ?>
