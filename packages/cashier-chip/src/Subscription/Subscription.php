de26672d81cabb687bc4982be404a12a4949c20e
Saiffil Fariz <1634949+sairiz@users.noreply.github.com>
2026-06-28 16:06:05 +0800

    Update Subscription.php

--- packages/cashier-chip/src/Subscription/Subscription.php
+++ packages/cashier-chip/src/Subscription/Subscription.php
@@ -106,6 +106,8 @@ class Subscription extends Model
         'billable_type',
         'billable_id',
         'type',
+        'chip_id',
+        'chip_status',
         'chip_price',
         'quantity',
         'recurring_token',
